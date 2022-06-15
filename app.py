9d5ba756541bf6444551eb5ee02a337bd52466e8
B S Abhishek <67589384+Abhishekbestha@users.noreply.github.com>
2022-06-15 15:10:18 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,34 @@
+from flask import Flask, request
+
+app= Flask(__name__)
+
+
+@app.route("/home", methods=["POST"])
+def home():
+    content= request.json
+    # print(content)
+    text= content["text"]
+    if text.isalpha()==True:
+
+        return "welcome"
+
+    else:
+        return "Bye"
+
+
+
+@app.route("/user", methods=["POST"])
+def user():
+    content= request.json
+
+    text= content["Name"]
+    if text=="Abhi":
+
+        return {"Response Message":"welcome Boss"},200
+
+    else:
+        return {"Response Message":"Who are you mad fellow?"}
+
+
+if __name__== "__main__":
+    app.run()
