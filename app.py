0c5630169d204b5b2fd6a83d8354d426b940bc44
B S Abhishek <67589384+Abhishekbestha@users.noreply.github.com>
2022-06-15 15:45:05 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,59 @@
+from flask import Flask, request
+import os
+
+app= Flask(__name__)
+
+
+@app.route("/home", methods=["POST"])
+def home():
+    content= request.json
+    # print(content)
+    text= content["text"]
+    if text.isalpha()==True:
+
+        return "welcome"
+
+    else:
+        return "Bye"
+
+# <----------------------------------------------------------->
+
+@app.route("/user", methods=["POST"])
+def user():
+    content= request.json
+
+    text= content["Name"]
+    if text=="Abhi":
+
+        return {"Response Message":"welcome Boss"},200
+
+    else:
+        return {"Response Message":"Who are you mad fellow?"}
+
+# <------------------------------------------------------------------------>
+
+def save_img(text, image):
+    # if not os.path.exists("C:/Users/21701/Downloads"+"/"+text):
+    #     os.mkdir("C:/Users/21701/Downloads"+"/"+text)
+    # img1_file= open("C:/Users/21701/Downloads"+"/"+text+"/"+text+".jpg", "wb")
+    img=image.read()
+    img1_file= open("C:/Users/21701/Downloads"+"/"+text+".jpg", "wb")
+    img1_file.write(img)
+
+    return "Succesfully saved the image"
+
+@app.route("/img", methods=["POST"])
+def img():
+    imagefile = request.files
+    textfile = request.form
+    text= textfile["text"]
+    image= imagefile["image"]
+    save_img(text, image)
+
+    return "Success"
+
+
+
+
+if __name__== "__main__":
+    app.run()
