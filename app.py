481a83eeb06a701508ac2a1d7ebaececbaf15c2a
B S Abhishek <67589384+Abhishekbestha@users.noreply.github.com>
2022-06-16 00:08:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request
-
+import os
+from datetime import datetime
+import hashlib
 
 app= Flask(__name__)
 
@@ -37,7 +39,7 @@ def save_img(text, image):
     #     os.mkdir("C:/Users/21701/Downloads"+"/"+text)
     # img1_file= open("C:/Users/21701/Downloads"+"/"+text+"/"+text+".jpg", "wb")
     img=image.read()
-    img1_file= open("/flaskapi/data"+"/"+text+".jpg", "wb")
+    img1_file= open("C:/Users/21701/Downloads"+"/"+text+".jpg", "wb")
     img1_file.write(img)
 
     return "Succesfully saved the image"
@@ -49,10 +51,29 @@ def img():
     text= textfile["text"]
     image= imagefile["image"]
     save_img(text, image)
+    my_date = datetime.now()
+    ts = my_date.isoformat().split(".")[0]
+    return {'TS':ts,'Response msg': "Image saved"}
 
-    return "Image saved Successfully"
+# <-------------------------------------------------------------------------->
 
 
+@app.route("/time", methods=["GET"])
+def time():
+    my_date = datetime.now()
+    txn = my_date.strftime('%Y%m%d%H%M%S')
+    ts = my_date.isoformat().split(".")[0]
+    accesskey = "Desktop_App"
+    sha256_hash = hashlib.sha256()
+    a_string = accesskey + str(ts) + str(txn)
+    encoded_string = a_string.encode()
+    byte_array = bytearray(encoded_string)
+    sha256_hash.update(byte_array)
+    hash = sha256_hash.hexdigest()
+    content= request.json
+
+
+    return {"ts":ts,"txn":txn,"hash":hash}
 
 
 if __name__== "__main__":
