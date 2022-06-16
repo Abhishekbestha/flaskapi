f890b21f426f25caa96f016faba0a5ea16d1f318
B S Abhishek <67589384+Abhishekbestha@users.noreply.github.com>
2022-06-16 10:04:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -58,15 +58,15 @@ def img():
 # <-------------------------------------------------------------------------->
 
 
-@app.route("/time", methods=["GET"])
+@app.route("/time", methods=["POST"])
 def time():
-#     content= request.form
+    content= request.form
 
     my_date = datetime.now()
     txn = my_date.strftime('%Y%m%d%H%M%S')
     ts = my_date.isoformat().split(".")[0]
-#     accesskey = content["accesskey"]
-    accesskey = "${{secrets.ACCESSKEY}}"
+    accesskey = content["accesskey"]
+#     accesskey = "${{secrets.ACCESSKEY}}"
     sha256_hash = hashlib.sha256()
     a_string = accesskey + str(ts) + str(txn)
     encoded_string = a_string.encode()
